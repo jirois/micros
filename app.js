d36b64e1f2a60c6fb0e0944a0f8970ed7470f18f
Omas Ajiri <omasajiri@gmail.com>
2022-11-01 16:45:02 +0100

    fix: completed setup

--- app.js
+++ app.js
@@ -3,13 +3,14 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 dotenv.config();
-import bodyParser, { urlencoded } from "body-parser";
 import helmet from "helmet";
+import cookieParser from "cookie-parser";
 const app = express();
 
+app.use(express.json());
 app.use(cors());
-app.use(bodyParser(urlencoded({ type: true })));
 app.use(helmet());
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.send("Welcome home!");
