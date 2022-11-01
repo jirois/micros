d01bdb1a598f5da37121d6f7f1ceae90d1e21777
Omas Ajiri <omasajiri@gmail.com>
2022-11-01 17:30:54 +0100

    feat: added models

--- models/User.js
+++ models/User.js
@@ -1,20 +1,55 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+import validator from "validator";
 
-const userSchema = new mongoose.Schema({
+const UserSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true,
+    required: [true, "Please provide name"],
+    minlength: 3,
+    maxlength: 50,
   },
   email: {
     type: String,
     unique: true,
-    required: true,
+    required: [true, "Please provide email"],
+    validate: {
+      validator: validator.isEmail,
+      message: "Please provide a valid email address",
+    },
   },
   password: {
     type: String,
-    required: true,
+    required: [true, "Please provide password"],
+    minlength: 6,
   },
+
+  role: {
+    type: String,
+    enum: ["admin", "user"],
+    default: "user",
+  },
+  isVerified: {
+    type: Boolean,
+    default: false,
+  },
+  verified: Date,
+  passwordToken: {
+    type: String,
+  },
+  passwordTokenExpirationDate: Date,
 });
+
+UserSchema.pre("save", async function () {
+  if (!this.isModified("password")) return;
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+UserSchema.methods.comparePassword = async function (canditatePassword) {
+  const isMatch = await bcrypt.compare(canditatePassword, this.password);
+  return isMatch;
+};
 
 const User = mongoose.model("User", userSchema);
 
